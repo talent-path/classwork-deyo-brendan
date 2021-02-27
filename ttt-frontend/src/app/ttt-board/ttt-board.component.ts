b0e066545615cce8df9228053b820f9d1a7a3566
Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
2021-02-27 11:00:46 -0500

    Completed TTT-FrontEnd Application

--- ttt-frontend/src/app/ttt-board/ttt-board.component.ts
+++ ttt-frontend/src/app/ttt-board/ttt-board.component.ts
@@ -3,6 +3,7 @@ import { Board, TTTBoard } from '../ttt/Board';
 import { Position } from '../ttt/Position';
 import { TTTSymbol } from '../ttt/TTTSymbol';
 import { Move } from '../ttt/Move';
+import { $ } from 'protractor';
 
 
 @Component({
@@ -12,6 +13,11 @@ import { Move } from '../ttt/Move';
 })
 export class TttBoardComponent implements OnInit {
 
+  xWins : number = 0;
+  oWins : number = 0;
+  nDraws : number = 0;
+  oldDiv = document.getElementById("win");
+  resetBoard : Board = new TTTBoard();
   isDraw: boolean = false;
   isWinner: boolean = false;
   boxCount: number = 0;
@@ -37,23 +43,62 @@ export class TttBoardComponent implements OnInit {
         if (this.tttBoard.isXTurn) 
         {
           console.log("Player O Wins");
+          this.oWins++;
+          let node = document.createElement("h1");
+          let text = document.createTextNode("Player O Wins!");
+          node.appendChild(text);
+          document.getElementById("win").appendChild(node);
+          this.printStats();
         }
-        else
+        else 
+        {
           console.log("Player X Wins");
+          this.xWins++;
+          let node = document.createElement("h1");
+          let text = document.createTextNode("Player X Wins!");
+          node.appendChild(text);
+          document.getElementById("win").appendChild(node);
+          this.printStats();
+        }
       }
       // draw
       if (!TttBoardComponent.getWinner(newBoard) && this.boxCount === 9) 
       {
           console.log("Draw");
+          this.nDraws++;
+          let node = document.createElement("h1");
+          let text = document.createTextNode("DRAW");
+          node.appendChild(text);
+          document.getElementById("win").appendChild(node);
+          this.printStats();
       }
     }
   }
 
   startNewGame(): void {
-    this.isWinner = false;
-    this.isDraw = false;
+    
+    this.tttBoard = this.resetBoard;
     this.boxCount = 0;
-    this.tttBoard.isXTurn = true;
+    document.getElementById("win").innerHTML = "";
+    document.getElementById("stats").innerHTML = "";
+
+  }
+
+  printStats() : void {
+
+    let statNode = document.createElement("h3");
+    let statText = document.createTextNode("Number of X Wins: " + this.xWins);
+    statNode.appendChild(statText);
+    document.getElementById("stats").appendChild(statNode);
+    let statNode2 = document.createElement("h3");
+    let statText2 = document.createTextNode("Number of O Wins: " + this.oWins);
+    statNode2.appendChild(statText2);
+    document.getElementById("stats").appendChild(statNode2);
+    let statNode3 = document.createElement("h3");
+    let statText3 = document.createTextNode("Number of Draws: " + this.nDraws);
+    statNode3.appendChild(statText3);
+    document.getElementById("stats").appendChild(statNode3);
+
 
   }
 
@@ -70,28 +115,36 @@ export class TttBoardComponent implements OnInit {
     //   [2, 4, 6]
     // ];
 
-    if ((board.allSquares[0][0] &&  board.allSquares[0][1] && board.allSquares[0][2]) && (board.allSquares[0][0].isX == board.allSquares[0][1].isX 
+    if ((board.allSquares[0][0] &&  board.allSquares[0][1] && board.allSquares[0][2]) 
+        && (board.allSquares[0][0].isX == board.allSquares[0][1].isX 
         && board.allSquares[0][0].isX == board.allSquares[0][2].isX)) {
       return true;
-    } else if ((board.allSquares[0][0] && board.allSquares[1][1] && board.allSquares[2][2]) && (board.allSquares[0][0].isX == board.allSquares[1][1].isX 
+    } else if ((board.allSquares[0][0] && board.allSquares[1][1] && board.allSquares[2][2]) 
+        && (board.allSquares[0][0].isX == board.allSquares[1][1].isX 
         && board.allSquares[0][0].isX == board.allSquares[2][2].isX)) {
       return true;
-    } else if ((board.allSquares[0][0] && board.allSquares[1][0] && board.allSquares[2][0]) && (board.allSquares[0][0].isX == board.allSquares[1][0].isX 
+    } else if ((board.allSquares[0][0] && board.allSquares[1][0] && board.allSquares[2][0]) 
+        && (board.allSquares[0][0].isX == board.allSquares[1][0].isX 
         && board.allSquares[0][0].isX == board.allSquares[2][0].isX)) {
       return true;
-    } else if ((board.allSquares[2][0] && board.allSquares[2][1] && board.allSquares[2][2]) && (board.allSquares[2][0].isX == board.allSquares[2][1].isX 
+    } else if ((board.allSquares[2][0] && board.allSquares[2][1] && board.allSquares[2][2]) 
+        && (board.allSquares[2][0].isX == board.allSquares[2][1].isX 
         && board.allSquares[2][0].isX == board.allSquares[2][2].isX)) {
       return true;
-    } else if ((board.allSquares[2][0] && board.allSquares[1][1] && board.allSquares[0][2]) && (board.allSquares[2][0].isX == board.allSquares[1][1].isX 
+    } else if ((board.allSquares[2][0] && board.allSquares[1][1] && board.allSquares[0][2]) 
+        && (board.allSquares[2][0].isX == board.allSquares[1][1].isX 
         && board.allSquares[2][0].isX == board.allSquares[0][2].isX)) {
       return true;
-    } else if ((board.allSquares[0][2] && board.allSquares[1][2] && board.allSquares[2][2]) && (board.allSquares[0][2].isX == board.allSquares[1][2].isX 
+    } else if ((board.allSquares[0][2] && board.allSquares[1][2] && board.allSquares[2][2]) 
+        && (board.allSquares[0][2].isX == board.allSquares[1][2].isX 
         && board.allSquares[0][2].isX == board.allSquares[2][2].isX)) {
       return true;
-    } else if ((board.allSquares[0][1] && board.allSquares[1][1] && board.allSquares[2][1]) && (board.allSquares[0][1].isX == board.allSquares[1][1].isX 
+    } else if ((board.allSquares[0][1] && board.allSquares[1][1] && board.allSquares[2][1]) 
+        && (board.allSquares[0][1].isX == board.allSquares[1][1].isX 
         && board.allSquares[0][1].isX == board.allSquares[2][1].isX)) {
       return true;
-    } else if ((board.allSquares[1][0] && board.allSquares[1][1] && board.allSquares[1][2]) && (board.allSquares[1][0].isX == board.allSquares[1][1].isX 
+    } else if ((board.allSquares[1][0] && board.allSquares[1][1] && board.allSquares[1][2]) 
+        && (board.allSquares[1][0].isX == board.allSquares[1][1].isX 
         && board.allSquares[1][0].isX == board.allSquares[1][2].isX)) {
       return true;
     }
