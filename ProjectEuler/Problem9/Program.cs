54456b73575eacc0cc98e2305b527fbf307e3b33
Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
2021-05-26 15:41:22 -0400

    rpg game

---
+++ ProjectEuler/Problem9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Utils;
+
+namespace Problem9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool solved = false;
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            int sum = 1000;
+
+            while (!solved)
+            {
+                for (a = 1; a < sum / 4; a++)
+                {
+                    for (b = a; b < sum / 2; b++)
+                    {
+                        c = sum - a - b;
+                        if (a * a + b * b == c * c)
+                        {
+                            solved = true;
+                            break;
+                        }
+                    }
+
+                    if (solved && (a + b + c == sum))
+                    {
+                        Console.WriteLine("ANSWER");
+                        Console.WriteLine("a: " + a);
+                        Console.WriteLine("b: " + b);
+                        Console.WriteLine("c: " + c);
+                        Console.WriteLine(a * b * c);
+                    }
+
+                }
+            }
+        }
+    }
+}
